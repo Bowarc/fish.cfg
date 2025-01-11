01d4612c1f9001015b2ebadf1af95b33b619cdc9
Bowarc <yuyeaaaahhhh@laposte.net>
2025-01-11 17:20:02 +0100

    Initial commit

---
+++ config.fish
@@ -0,0 +1,48 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# User specific environment
+if not contains "$HOME/.local/bin:$HOME/bin" $PATH
+    set -gx PATH "$HOME/.local/bin:$HOME/bin" $PATH
+end
+set -gx PATH "/home/bowarc/.cargo/bin" $PATH
+
+# Initialize the z command
+zoxide init fish | source
+
+# Init oh my posh
+oh-my-posh init fish --config ~/.config/oh-my-posh/pwsh10k.omp.json | source
+
+# Flatpak aliases
+alias discord="flatpak run dev.vencord.Vesktop"
+alias osu="flatpak run sh.ppy.osu"
+alias easyeffects="flatpak run com.github.wwmm.easyeffects"
+alias spotube="flatpak run com.github.KRTirtho.Spotube"
+alias obs="flatpak run com.obsproject.Studio"
+
+# Other aliases
+alias ls="eza -las type"
+alias cat="bat"
+
+function gdiff
+    # Change to the project root directory
+    set project_root (git rev-parse --show-toplevel)
+    cd $project_root
+
+    # Use fzf to select files and show diffs
+    git diff --name-only | fzf --multi --ansi --preview-window right:70% --preview "git diff --color=always -- {}" --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down"
+
+    # Return to the previous directory
+    # cd -
+end
+
+# Remove fish hello message 
+set -U fish_greeting
+
+# Custom error message on bad command 
+function fish_command_not_found
+    # __fish_default_command_not_found_handler $argv
+    command_not_found
+    # echo Fish: Unknown command: $argv
+end
---
+++ functions/command_not_found.fish
@@ -0,0 +1,41 @@
+function command_not_found
+
+    set messages {
+"Boooo!",
+"Don't you know anything?",
+"RTFM!",
+"Hahaha, n00b!",
+"Wow! That was impressively wrong!",
+"He said: $argv :D",
+"What are you doing??",
+"Pathetic",
+"...and this is the best you can do??",
+"The worst one today!",
+"n00b alert!",
+"Your application for reduced salary has been sent!",
+"lol",
+"u suk",
+"lol... plz",
+"plz uninstall",
+"And the Darwin Award goes to.... $USER!",
+"ERROR_INCOMPETENT_USER",
+"Incompetence is also competence",
+"Bad.",
+"Fake it till you make it!",
+"What is this...? $argv? Amateur hour!?",
+"Come on! You can do it!",
+"Nice try.",
+"What if... you type an actual command the next time!",
+"What if I told you... it is possible to type valid commands.",
+"Y u no speak computer???",
+"This is not Mac",
+"Perhaps you should leave the command line alone...",
+"Please step away from the keyboard!",
+"error code: 1D10T",
+"ACHTUNG! ALLES TURISTEN UND NONTEKNISCHEN LOOKENPEEPERS! DAS KOMPUTERMASCHINE IST NICHT FÜR DER GEFINGERPOKEN UND MITTENGRABEN! ODERWISE IST EASY TO SCHNAPPEN DER SPRINGENWERK, BLOWENFUSEN UND POPPENCORKEN MIT SPITZENSPARKEN. IST NICHT FÜR GEWERKEN BEI DUMMKOPFEN. DER RUBBERNECKEN SIGHTSEEREN KEEPEN DAS COTTONPICKEN HÄNDER IN DAS POCKETS MUSS. ZO RELAXEN UND WATSCHEN DER BLINKENLICHTEN.",
+"Pro tip: type a valid command!"
+}
+    set_color red
+    echo \r(random choice $messages)
+    set_color normal
+end
