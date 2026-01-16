35863c382f52936230ae519853d5a9ff12012f39
Bowarc <yuyeaaaahhhh@laposte.net>
2026-01-16 14:25:06 +0100

    Fixing visual bug in gcd

--- functions/gcd.fish
+++ functions/gcd.fish
@@ -1,5 +1,11 @@
 function gcd
     set root $(git rev-parse --show-toplevel)
+
+    if [ $root = $(pwd) ]
+        echo "Already at root"
+        return
+    end
+
     echo -e "$root/\033[31m$(string replace $root"/" "" $(pwd))/\033"
     cd $root
 end
